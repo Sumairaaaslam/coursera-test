a466c4596114222daed410f36983c5a74855e60b
Sumaira Aslam <sumairaaaslam@gmail.com>
2023-03-03 17:26:03 +0500

    New file

---
+++ mod4/js/script.js
@@ -0,0 +1,11 @@
+// function do(){
+// 	var name=document.getElementById("txt").value;
+//     var message="<h2>Parhlo " + name + "ye links kholne se ghar nhi chlte</h2>";
+//     document.getElementById("content").innerHTML=message;
+// }
+
+function subt(){
+	var name= document.getElementById("txt").value;
+	var message="<h2>Parhlo '" + name + "' faltu links kholne se ghar nhi chlte :(</h2>";
+	document.getElementById("content").innerHTML= message;
+}
